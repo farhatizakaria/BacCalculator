1b4a3bade2a6c399110997fa03197e4bf0c96cae
Zakaria Farhati <zakariafarhati18@gmail.com>
2020-02-02 18:47:37 +0100

    First Commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,221 @@
+﻿namespace BacCalculator
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxCC = new System.Windows.Forms.TextBox();
+            this.textBoxRE = new System.Windows.Forms.TextBox();
+            this.textBoxNE = new System.Windows.Forms.TextBox();
+            this.buttonExit = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.MoyGroup = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.labelResult = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.bunifuCustomLabel1 = new Bunifu.Framework.UI.BunifuCustomLabel();
+            this.MoyGroup.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBoxCC
+            // 
+            this.textBoxCC.Location = new System.Drawing.Point(233, 37);
+            this.textBoxCC.Name = "textBoxCC";
+            this.textBoxCC.Size = new System.Drawing.Size(89, 25);
+            this.textBoxCC.TabIndex = 2;
+            // 
+            // textBoxRE
+            // 
+            this.textBoxRE.Location = new System.Drawing.Point(233, 68);
+            this.textBoxRE.Name = "textBoxRE";
+            this.textBoxRE.Size = new System.Drawing.Size(89, 25);
+            this.textBoxRE.TabIndex = 3;
+            // 
+            // textBoxNE
+            // 
+            this.textBoxNE.Location = new System.Drawing.Point(233, 99);
+            this.textBoxNE.Name = "textBoxNE";
+            this.textBoxNE.Size = new System.Drawing.Size(89, 25);
+            this.textBoxNE.TabIndex = 4;
+            // 
+            // buttonExit
+            // 
+            this.buttonExit.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.buttonExit.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.buttonExit.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.buttonExit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonExit.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.buttonExit.Location = new System.Drawing.Point(485, 355);
+            this.buttonExit.Name = "buttonExit";
+            this.buttonExit.Size = new System.Drawing.Size(81, 27);
+            this.buttonExit.TabIndex = 5;
+            this.buttonExit.Text = "Exit";
+            this.buttonExit.UseVisualStyleBackColor = true;
+            this.buttonExit.Click += new System.EventHandler(this.buttonExit_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label3.Location = new System.Drawing.Point(12, 37);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 22);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "C.C";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label4.Location = new System.Drawing.Point(12, 67);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(140, 22);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Regional Exam";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label5.Location = new System.Drawing.Point(12, 98);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(140, 22);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "National Exam";
+            // 
+            // MoyGroup
+            // 
+            this.MoyGroup.BackColor = System.Drawing.Color.Teal;
+            this.MoyGroup.Controls.Add(this.labelResult);
+            this.MoyGroup.Controls.Add(this.button1);
+            this.MoyGroup.Controls.Add(this.label6);
+            this.MoyGroup.Controls.Add(this.textBoxCC);
+            this.MoyGroup.Controls.Add(this.label5);
+            this.MoyGroup.Controls.Add(this.textBoxRE);
+            this.MoyGroup.Controls.Add(this.label4);
+            this.MoyGroup.Controls.Add(this.textBoxNE);
+            this.MoyGroup.Controls.Add(this.label3);
+            this.MoyGroup.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.MoyGroup.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.MoyGroup.Location = new System.Drawing.Point(130, 72);
+            this.MoyGroup.Name = "MoyGroup";
+            this.MoyGroup.Size = new System.Drawing.Size(361, 238);
+            this.MoyGroup.TabIndex = 9;
+            this.MoyGroup.TabStop = false;
+            this.MoyGroup.Text = "Moy.Gen";
+            // 
+            // button1
+            // 
+            this.button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1.Location = new System.Drawing.Point(223, 153);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(108, 32);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Calculate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // labelResult
+            // 
+            this.labelResult.AutoSize = true;
+            this.labelResult.Location = new System.Drawing.Point(233, 205);
+            this.labelResult.Name = "labelResult";
+            this.labelResult.Size = new System.Drawing.Size(88, 18);
+            this.labelResult.TabIndex = 10;
+            this.labelResult.Text = "The result";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label6.Location = new System.Drawing.Point(12, 157);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(100, 22);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Calculate";
+            // 
+            // bunifuCustomLabel1
+            // 
+            this.bunifuCustomLabel1.AutoSize = true;
+            this.bunifuCustomLabel1.Font = new System.Drawing.Font("Consolas", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bunifuCustomLabel1.ForeColor = System.Drawing.Color.Salmon;
+            this.bunifuCustomLabel1.Location = new System.Drawing.Point(220, 27);
+            this.bunifuCustomLabel1.Name = "bunifuCustomLabel1";
+            this.bunifuCustomLabel1.Size = new System.Drawing.Size(150, 22);
+            this.bunifuCustomLabel1.TabIndex = 10;
+            this.bunifuCustomLabel1.Text = "BAC CALCULATOR";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Teal;
+            this.ClientSize = new System.Drawing.Size(577, 393);
+            this.Controls.Add(this.bunifuCustomLabel1);
+            this.Controls.Add(this.MoyGroup);
+            this.Controls.Add(this.buttonExit);
+            this.Font = new System.Drawing.Font("Consolas", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "MainForm";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Main";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MoyGroup.ResumeLayout(false);
+            this.MoyGroup.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxCC;
+        private System.Windows.Forms.TextBox textBoxRE;
+        private System.Windows.Forms.TextBox textBoxNE;
+        private System.Windows.Forms.Button buttonExit;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.GroupBox MoyGroup;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label labelResult;
+        private System.Windows.Forms.Label label6;
+        private Bunifu.Framework.UI.BunifuCustomLabel bunifuCustomLabel1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BacCalculator
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            if (textBoxNE.Text == "" || textBoxRE.Text == "" || textBoxCC.Text == "")
+            {
+                MessageBox.Show("Something is wrong, please fill all text boxes");
+            }
+            else
+            {
+                try
+                {
+
+                    double NE = double.Parse(textBoxNE.Text);
+                    double RE = double.Parse(textBoxRE.Text);
+                    double CC = double.Parse(textBoxCC.Text);
+                    double R = ((NE * 0.5) + (RE * 0.25) + (CC * 0.25));
+                    //R.ToString();
+                    labelResult.Text = R.ToString();
+
+                }
+
+                catch (Exception)
+                {
+
+                    MessageBox.Show(" the user should write \",\" in place of \".");
+                }
+            }
+        }
+
+    }
+}
